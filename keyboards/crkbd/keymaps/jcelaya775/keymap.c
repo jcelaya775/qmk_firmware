ec1a6e4282a27b689c6793cf4f637fdf041987b1
Jorge Celaya <jcelaya775@gmail.com>
2024-01-21 21:37:56 -0600

    Double-tap caps progress

--- keyboards/crkbd/keymaps/jcelaya775/keymap.c
+++ keyboards/crkbd/keymaps/jcelaya775/keymap.c
@@ -7,61 +7,42 @@
  */
 
 // enum {
-//     TD_CW_TOGG_CAPS,
-// };
-//
-// // Tap Dance definitions
-// tap_dance_action_t tap_dance_actions[] = {
-//     // Tap once for CW_TOGG, twice for Caps Lock
-//     [TD_CW_TOGG_CAPS] = ACTION_TAP_DANCE_DOUBLE(CW_TOGG, KC_CAPS),
-// };
-
-// enum tap_dance_codes {
-//   DANCE_0,
-// };
-//
-// typedef struct {
-//     bool is_press_action;
-//     uint8_t step;
-// } tap;
-//
-// enum {
 //     SINGLE_TAP = 1,
-//     SINGLE_HOLD,
 //     DOUBLE_TAP,
-//     DOUBLE_HOLD,
-//     DOUBLE_SINGLE_TAP,
 //     MORE_TAPS
 // };
 //
-// static tap dance_state[23];
+// enum {
+//     DANCE_0,
+// };
 //
+// // TODO: qk_tap_dance_state_t is an undefined type
 // uint8_t dance_step(qk_tap_dance_state_t *state);
 //
 // uint8_t dance_step(qk_tap_dance_state_t *state) {
 //     if (state->count == 1) {
 //         if (state->interrupted || !state->pressed) return SINGLE_TAP;
-//         else return SINGLE_HOLD;
+//         // else return SINGLE_HOLD;
 //     } else if (state->count == 2) {
-//         if (state->interrupted) return DOUBLE_SINGLE_TAP;
-//         else if (state->pressed) return DOUBLE_HOLD;
-//         else return DOUBLE_TAP;
+//         // if (state->interrupted) return DOUBLE_SINGLE_TAP;
+//         // else if (state->pressed) return DOUBLE_HOLD;
+//         if (state->interrupted || state->pressed)
+//             return DOUBLE_TAP;
 //     }
 //     return MORE_TAPS;
 // }
-//
 //
 // void on_dance_0(qk_tap_dance_state_t *state, void *user_data);
 // void dance_0_finished(qk_tap_dance_state_t *state, void *user_data);
 // void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
 //
 // void on_dance_0(qk_tap_dance_state_t *state, void *user_data) {
-//     if(state->count == 2) {
+//     if(state->count == 1) {
 //         tap_code16(KC_CAPS);
 //         tap_code16(KC_CAPS);
 //         tap_code16(KC_CAPS);
 //     }
-//     if(state->count > 2) {
+//     if(state->count > 3) {
 //         tap_code16(KC_CAPS);
 //     }
 // }
@@ -70,47 +51,56 @@
 //     dance_state[0].step = dance_step(state);
 //     switch (dance_state[0].step) {
 //         case SINGLE_TAP: register_code16(KC_CAPS); break;
-//         case SINGLE_HOLD: register_code16(KC_CAPS); break;
-//         case DOUBLE_TAP: register_code16(CW_TOGG); register_code16(CW_TOGG); break;
-//         case DOUBLE_SINGLE_TAP: tap_code16(CW_TOGG); register_code16(CW_TOGG);
+//         case DOUBLE_TAP: register_code16(KC_CAPS); register_code16(KC_CAPS); break;
 //     }
 // }
 //
 // void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
 //     wait_ms(10);
 //     switch (dance_state[0].step) {
-//         case SINGLE_TAP: unregister_code16(KC_EQUAL); break;
-//         case SINGLE_HOLD: unregister_code16(KC_INSERT); break;
-//         case DOUBLE_TAP: unregister_code16(KC_EQUAL); break;
-//         case DOUBLE_SINGLE_TAP: unregister_code16(KC_EQUAL); break;
+//         case SINGLE_TAP: unregister_code16(KC_CAPS); break;
+//         case DOUBLE_TAP: unregister_code16(KC_CAPS); break;
 //     }
 //     dance_state[0].step = 0;
 // }
-
-
-// enum combo_events {
-//   CAPS_COMBO,
-// };
 //
-// const uint16_t caps_combo[] PROGMEM = {CW_TOGG, KC_LSFT};
+// qk_tap_dance_action_t tap_dance_actions[] = {
+//     [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset)
+// }
 //
-// combo_t key_combos[] = {
-//   [CAPS_COMBO] = COMBO_ACTION(caps_combo),
-//   // Other combos...
-// };
+// bool caps_word_press_user(uint16_t keycode) {
+//     switch (keycode) {
+//         // Keycodes that continue Caps Word, with shift applied.
+//         case KC_A ... KC_Z:
+//         //case KC_MINS: // CHANGE - do not shift -_ (see below)
+//             add_weak_mods(MOD_BIT(KC_LSFT)); // Apply shift to next key.
+//             send_keyboard_report();
+//             return true;
 //
-// void process_combo_event(uint16_t combo_index, bool pressed) {
-//   switch(combo_index) {
-//     case CAPS_COMBO:
-//       if (pressed) {
-//         register_code(KC_CAPS);
-//       }
-//       break;
-//   }
+//         // Keycodes that continue Caps Word, without shifting.
+//         case KC_MINS: // CHANGE - default is -_ is shifted.
+//         case KC_1 ... KC_0:
+//         case KC_BSPC:
+//         case KC_DEL:
+//         case KC_UNDS:
+//         // Tapdance keys are handled manually.
+//         case TD(DANCE_0): // Should be changed to caps lock
+//             return true;
+//
+//         default:
+//             return false; // Deactivate Caps Word.
+//     }
 // }
 
+// // Tap Dance definitions
+// tap_dance_action_t tap_dance_actions[] = {
+//     // Tap once for CW_TOGG, twice for Caps Lock
+//     // TODO: Wrap CW_TOGG in a function that calls caps_word_on() b/c ?? (it don't work no other way)
+//     [TD_CW_TOGG_CAPS] = ACTION_TAP_DANCE_DOUBLE(CW_TOGG, KC_CAPS),
+// };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SC_SENT, KC_LCTL, MT(MOD_LGUI, KC_ENT), MO(1), MO(2), KC_SPC, KC_LALT),
-                                                              [1] = LAYOUT_split_3x6_3(KC_CAPS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_LCTL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_TILD, KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_BSLS, KC_LSFT, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, SC_SENT, KC_NO, KC_NO, KC_NO, CW_TOGG, KC_SPC, KC_LALT),
+                                                              [1] = LAYOUT_split_3x6_3(KC_CAPS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_LCTL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_TILD, KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_BSLS, KC_LSFT, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, SC_SENT, KC_NO, KC_NO, KC_NO, KC_CAPS, KC_SPC, KC_LALT),
                                                               [2] = LAYOUT_split_3x6_3(KC_TAB, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_LALT, TG(3), KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_F12, KC_LSFT, KC_NO, KC_NO, KC_MPRV, KC_MNXT, KC_MPLY, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, SC_SENT, KC_LCTL, KC_ENT, KC_LGUI, KC_NO, KC_NO, KC_NO),
                                                               [3] = LAYOUT_split_3x6_3(KC_TAB, TG(3), KC_NO, KC_NO, KC_NO, QK_BOOT, KC_PLUS, KC_7, KC_8, KC_9, KC_MINS, KC_BSPC, KC_ESC, KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, KC_ASTR, KC_4, KC_5, KC_6, KC_SLSH, KC_NO, KC_LSFT, KC_NO, RGB_HUD, RGB_SAD, RGB_VAD, RGB_MOD, KC_EQL, KC_1, KC_2, KC_3, KC_COMM, KC_NO, KC_LCTL, KC_ENT, KC_LALT, KC_NO, KC_SPC, KC_0)};
 
@@ -130,7 +120,7 @@ bool    process_record_user(uint16_t keycode, keyrecord_t *record) {
                 if (mod_state & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) {
                     // First temporarily canceling both shifts so that
                     // shift isn't applied to the KC_DEL keycode
-                    del_mods(MOD_BIT(KC_LSFT)); // allow for right shift to be used
+                    del_mods(MOD_BIT(KC_LSFT)); // allow for right shift to be used with delete
                     register_code(KC_DEL);
                     // Update the boolean variable to reflect the status of KC_DEL
                     delkey_registered = true;
