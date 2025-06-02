92adc5387860598af5035319d86dd0dbf3732969
Jorge Celaya <jcelaya775@gmail.com>
2025-06-01 22:18:51 -0500

    feat: mouse keys

--- keyboards/crkbd/keymaps/jcelaya775/keymap.c
+++ keyboards/crkbd/keymaps/jcelaya775/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 
+// [ ] TODO: Ctrl+MO(2) activates layer 3
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default layer
     [0] = LAYOUT_split_3x6_3(
@@ -19,16 +20,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_F1, KC_F2, KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,
         KC_LALT, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,  KC_F12,
-        KC_LSFT, KC_NO, KC_NO, KC_MPRV, KC_MNXT, KC_MPLY,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,  SC_SENT,
+        KC_LSFT, TO(3), KC_NO, KC_MPRV, KC_MNXT, KC_MPLY,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,  SC_SENT,
                   MT(MOD_LCTL, MOD_LGUI), MT(MOD_LGUI, KC_ENT), MO(1),      KC_NO, KC_NO, KC_NO
     ),
-    // number-pad layer (disabled for now)
+    // mouse layer
     [3] = LAYOUT_split_3x6_3(
-        KC_TAB,  TG(3), KC_NO,   KC_NO,   KC_NO,   QK_BOOT,         KC_ASTR, KC_7, KC_8, KC_9, KC_SLSH, KC_BSPC,
-        KC_ESC,  KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,         KC_PLUS, KC_4, KC_5, KC_6, KC_MINS, KC_ENT,
-        KC_LSFT, KC_NO, RGB_HUD, RGB_SAD, RGB_VAD, RGB_MOD,         KC_COMM, KC_1, KC_2, KC_3, KC_DOT,  KC_EQL,
-                                  KC_LCTL, KC_ENT, KC_LALT,         KC_COMM, KC_SPC, KC_0
-    )
+        KC_NO,  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_ESC,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,         KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_NO, KC_NO,
+        KC_NO, TG(3), KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,
+                                  KC_LCTL, KC_MS_BTN1, KC_NO,         MO(4), KC_MS_BTN1, KC_MS_BTN2
+    ),
+    // mouse scroll wheel layer
+    [4] = LAYOUT_split_3x6_3(
+        KC_NO,  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_ESC,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,         KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_RIGHT, KC_NO, KC_NO,
+        KC_NO, TG(3), KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                  KC_LCTL, KC_MS_BTN1, KC_NO,         KC_NO, KC_MS_BTN1, KC_MS_BTN2
+    ),
+    /* // mouse layer */
+    /* [3] = LAYOUT_split_3x6_3( */
+    /*     KC_NO,  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+    /*     TG(3),  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,         KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_NO, KC_NO, */
+    /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, */
+    /*                               KC_LCTL, KC_MS_BTN1, KC_NO,         KC_NO, KC_MS_BTN2, KC_0 */
+    /* ), */
+    // number-pad layer (disabled for now)
+    /* [3] = LAYOUT_split_3x6_3( */
+    /*     KC_TAB,  TG(3), KC_NO,   KC_NO,   KC_NO,   QK_BOOT,         KC_ASTR, KC_7, KC_8, KC_9, KC_SLSH, KC_BSPC, */
+    /*     KC_ESC,  KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,         KC_PLUS, KC_4, KC_5, KC_6, KC_MINS, KC_ENT, */
+    /*     KC_LSFT, KC_NO, RGB_HUD, RGB_SAD, RGB_VAD, RGB_MOD,         KC_COMM, KC_1, KC_2, KC_3, KC_DOT,  KC_EQL, */
+    /*                               KC_LCTL, KC_ENT, KC_LALT,         KC_COMM, KC_SPC, KC_0 */
 };
 
 // TODO: Move some of these variables closer to where they are used
